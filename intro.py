054dee2ac10d72e825f75012a755aec3c78fef0b
Poojitha Nagilla <103567616+Pooja290702@users.noreply.github.com>
2025-09-20 21:29:12 -0500

    Introduction to dictionaries

---
+++ intro.py
@@ -0,0 +1,13 @@
+name='pooja'
+age='22'
+gender='female'
+ #instead of creating 3 variables for 1 student, we can use dictionaries
+s1={
+    'name':'pooja',
+    'age':22,
+    'gender':'female',
+    'marks':[1,2,3,4,5],
+    #
+    }
+print(s1)
+print(type(s1))
