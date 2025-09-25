d054a63eb8869fbd24644eda7c34f8e61146da07
Poojitha Nagilla <103567616+Pooja290702@users.noreply.github.com>
2025-09-25 16:52:36 -0500

    Add files via upload

---
+++ empty_set.py
@@ -0,0 +1,7 @@
+my_set={}
+print(my_set)
+print(type(my_set))
+#the type of this empty set says it is a dictionary, 
+# so to make an empty set we need to use set()
+my_sets=set()
+print(type(my_sets))
